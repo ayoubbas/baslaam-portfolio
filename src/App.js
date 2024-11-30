1934d37d4fe4bc031f4ebda4dc890f461cfa4348
ayoubbas <ayoub-ok147@hotmail.fr>
2024-11-30 23:14:37 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "./styles/main.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <header className="header">
+        <h1 className="logo">hello</h1>
+
+        <nav className="navbar">
+          <ul className="navbar__list">
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">
+                Home
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">
+                services
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">
+                resume
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">
+                portfolio
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a className="navbar__link" href="#">
+                contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
