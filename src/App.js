e9e749eed6ddd211efc25c16f3305bfd044fbd3c
ayoubbas <ayoub-ok147@hotmail.fr>
2024-12-02 07:23:48 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "./styles/main.scss";
 import Home from "./components/Home";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Blog from "./components/Blog";
 
 function App() {
   return (
     <div className="App">
       <header className="header">
-        <h1 className="logo">hello</h1>
+        <div className="header__container">
+          <h1 className="logo">hello</h1>
 
-        <nav className="navbar">
-          <ul className="navbar__list">
-            <li className="navbar__item">
-              <a className="navbar__link" href="/">
-                Home
-              </a>
-            </li>
-            <li className="navbar__item">
-              <a className="navbar__link" href="/services">
-                services
-              </a>
-            </li>
-            <li className="navbar__item">
-              <a className="navbar__link" href="/resume">
-                resume
-              </a>
-            </li>
-            <li className="navbar__item">
-              <a className="navbar__link" href="/portfolio">
-                portfolio
-              </a>
-            </li>
-            <li className="navbar__item">
-              <a className="navbar__link" href="/contact">
-                contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+          <nav className="navbar">
+            <ul className="navbar__list">
+              <li className="navbar__item">
+                <Link className="navbar__link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="navbar__item">
+                <Link className="navbar__link" to="/services">
+                  Services
+                </Link>
+              </li>
+              <li className="navbar__item">
+                <Link className="navbar__link" to="/portfolio">
+                  portfolio
+                </Link>
+              </li>
+              <li className="navbar__item">
+                <Link className="navbar__link" to="/Blog">
+                  Blog
+                </Link>
+              </li>
+              <li className="navbar__item">
+                <Link className="navbar__link" to="/contact">
+                  contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
