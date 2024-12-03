b48f37f6313360f7e61df17121163dd1d4179640
ayoubbas <ayoub-ok147@hotmail.fr>
2024-12-03 22:10:05 +0100

    update

--- src/App.js
+++ src/App.js
@@ -14,21 +14,23 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="header">
-        <div className="header__container">
-          <h1 className="header__logo">Baslaam</h1>
-          <Navbar/>
-          <FontAwesomeIcon icon={faBars} className="menu-bar" />
-        </div>
-      </header>
+      <main className="home">
+        <header className="header">
+          <div className="header__container">
+            <h1 className="header__logo">Baslaam</h1>
+            <Navbar />
+            <FontAwesomeIcon icon={faBars} className="menu-bar" />
+          </div>
+        </header>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/Blog" element={<Blog />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
     </div>
   );
 }
